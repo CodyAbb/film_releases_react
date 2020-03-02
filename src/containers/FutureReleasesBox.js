6e17a3346cdbcfb8ddda1c719649889f89be86e5
Cody Abbott <cody.abbott16@gmail.com>
2020-03-02 15:26:41 +0000

    basic container setup with welcome message

---
+++ src/containers/FutureReleasesBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class FutureRealeasesBox extends Component {
+  render() {
+    return <h1>Upcoming film releases for the UK</h1>;
+  }
+}
+
+export default FutureRealeasesBox;
